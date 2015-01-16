b0e49b3d9f333e3ff2df65279002c03b9b6412ba
WilliamCipriano <williamfcipriano@gmail.com>
2015-01-16 15:13:45 -0500

    Update OLID.py
    Add better menu for login.

--- OLID.py
+++ OLID.py
@@ -2,6 +2,70 @@ import OpenDental
 import time
 import sys
 
+def manual():
+
+    def yorn(data):
+        if data == 'Y' or data == 'y':
+            return True
+        elif data == 'N' or data == 'n':
+            return False
+        else:
+            print 'Invalid Input!'
+            return False
+
+    print "Attempt to load previously saved configuration?"
+    check = raw_input('Y/N?')
+    if yorn(check):
+        if OpenDental.DatabaseConnection():
+            return True
+
+
+    def write(host, username, password, database, path):
+        try:
+            f = open(path, 'w')
+            f.write('Host = ' + host + '\nUsername = ' + username + '\nPassword = ' + password + '\nDatabase = ' + database)
+            f.close()
+            return True
+        except Exception as ex:
+            return False
+
+    print "Please enter OpenDental database details."
+    host = raw_input('Host?:')
+    username = raw_input('Username?')
+    password = raw_input('Password?')
+    database = raw_input('Database?')
+    print "Save this configuration?"
+    save = raw_input('Y/N?')
+    if yorn(save):
+        import os
+        path = os.path.dirname(__file__).replace('\\library.zip', "")
+        path += '\OpenDentalDatabaseConfig.ini'
+        if os.path.isfile(path):
+            print "configuration already exists! Overwrite?"
+            overwrite = raw_input('Y/N?')
+            if yorn(overwrite):
+                if write(host ,username ,password ,database, path):
+                    print "Parameters updated successfully."
+                else:
+                    print "Save failed."
+        else:
+            if write(host ,username ,password ,database, path):
+                print "Data saved."
+            else:
+                print "Save failed"
+
+    else:
+        print "Data not saved."
+
+
+    print 'Attempting to connect to database'
+    if OpenDental.DefineDatabase(host, username, password, database):
+        print "connection successful"
+        return True
+    else:
+        print "connection failed!"
+        return False
+
 
 def createuser():
     print "New User Wizard"
@@ -37,16 +101,7 @@ def deleteattachedclaims():
             print "claim deletion failed."
 
 
-
-print "OpenDental Login Identity Deceptor (OLID) - By: Will Cipriano"
-print "Attempting to connect to database... Please Wait"
-if (OpenDental.DatabaseConnection()):
-    print "Connection Successful"
-else:
-        print "database connection failed"
-        time.sleep(60)
-        sys.exit(False)
-
+manual()
 print "What would you like to do today?"
 print "1: Create User"
 print "2: Delete Attached Claims"
